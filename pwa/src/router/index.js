b5d5d248a79a354d2f321aba1462dbfa3c18969b
Craig Moss <craig@io.co.za>
2022-06-13 14:30:55 +0200

    Checking

--- pwa/src/router/index.js
+++ pwa/src/router/index.js
@@ -2,6 +2,16 @@ import { createRouter, createWebHistory } from "vue-router";
 import HomeView from "../views/HomeView.vue";
 
 const router = createRouter({
+  scrollBehavior(to, from, savedPosition) {
+    if (to.hash) {
+      return window.scrollTo({
+        top: document.querySelector(to.hash).offsetTop - 135,
+        behavior: "smooth",
+      });
+    } else {
+      return savedPosition || { x: 0, y: 0 };
+    }
+  },
   history: createWebHistory(import.meta.env.BASE_URL),
   routes: [
     {
