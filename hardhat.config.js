ba941ac22a1abc1f821e7a7b1744259fd94a2475
Craig Moss <craig@io.co.za>
2022-06-14 12:08:15 +0200

    Contract basics

--- hardhat.config.js
+++ hardhat.config.js
@@ -1,9 +1,9 @@
-require("dotenv").config();
-require("@nomiclabs/hardhat-waffle");
+require('dotenv').config();
+require('@nomiclabs/hardhat-waffle');
 
 // This is a sample Hardhat task. To learn how to create your own go to
 // https://hardhat.org/guides/create-task.html
-task("accounts", "Prints the list of accounts", async (taskArgs, hre) => {
+task('accounts', 'Prints the list of accounts', async (taskArgs, hre) => {
   const accounts = await hre.ethers.getSigners();
 
   for (const account of accounts) {
@@ -18,10 +18,10 @@ task("accounts", "Prints the list of accounts", async (taskArgs, hre) => {
  * @type import('hardhat/config').HardhatUserConfig
  */
 module.exports = {
-  defaultNetwork: "hardhat",
+  defaultNetwork: 'hardhat',
   networks: {
     localhost: {
-      url: "http://127.0.0.1:8545",
+      url: 'http://127.0.0.1:8545',
     },
     hardhat: {
       blockGasLimit: 1350000000,
@@ -29,84 +29,85 @@ module.exports = {
     },
     alchemy: {
       url: process.env.ALCHEMY_API_URL,
-      accounts: [process.env.ALCHEMY_API_KEY],
+      key: process.env.ALCHEMY_API_KEY,
+      accounts: [process.env.DEV_ACCOUNT],
     },
     testnet: {
-      url: "https://data-seed-prebsc-1-s1.binance.org:8545",
+      url: 'https://data-seed-prebsc-1-s1.binance.org:8545',
       chainId: 97,
       gasPrice: 20000000000,
       accounts: {
-        mnemonic: "test test test test test test test test test test test junk",
+        mnemonic: 'test test test test test test test test test test test junk',
         path: "m/44'/60'/0'/0",
         initialIndex: 0,
         count: 20,
-        passphrase: "",
+        passphrase: '',
       },
     },
     mainnet: {
-      url: "https://bsc-dataseed.binance.org/",
+      url: 'https://bsc-dataseed.binance.org/',
       chainId: 56,
       gasPrice: 20000000000,
       accounts: {
-        mnemonic: "test test test test test test test test test test test junk",
+        mnemonic: 'test test test test test test test test test test test junk',
         path: "m/44'/60'/0'/0",
         initialIndex: 0,
         count: 20,
-        passphrase: "",
+        passphrase: '',
       },
     },
     matic: {
-      url: "https://rpc-mainnet.matic.network",
+      url: 'https://rpc-mainnet.matic.network',
       accounts: {
-        mnemonic: "test test test test test test test test test test test junk",
+        mnemonic: 'test test test test test test test test test test test junk',
         path: "m/44'/60'/0'/0",
         initialIndex: 0,
         count: 20,
-        passphrase: "",
+        passphrase: '',
       },
       blockGasLimit: 20000000,
       gasPrice: 1000000000,
     },
     mumbai: {
-      url: "https://rpc-mumbai.matic.today",
+      url: 'https://rpc-mumbai.matic.today',
       accounts: {
-        mnemonic: "test test test test test test test test test test test junk",
+        mnemonic: 'test test test test test test test test test test test junk',
         path: "m/44'/60'/0'/0",
         initialIndex: 0,
         count: 20,
-        passphrase: "",
+        passphrase: '',
       },
       blockGasLimit: 20000000,
       gasPrice: 1000000000,
     },
     kovan: {
-      url: "https://kovan.infura.io/",
+      url: 'https://kovan.infura.io/',
       accounts: {
-        mnemonic: "test test test test test test test test test test test junk",
+        mnemonic: 'test test test test test test test test test test test junk',
         path: "m/44'/60'/0'/0",
         initialIndex: 0,
         count: 20,
-        passphrase: "",
+        passphrase: '',
       },
       gasPrice: 5000000000,
     },
     rinkeby: {
-      url: "https://eth-mainnet.alchemyapi.io/v2/123abc123abc123abc123abc123abcde",
+      url: 'https://eth-mainnet.alchemyapi.io/v2/123abc123abc123abc123abc123abcde',
       accounts: {
-        mnemonic: "test test test test test test test test test test test junk",
+        mnemonic: 'test test test test test test test test test test test junk',
         path: "m/44'/60'/0'/0",
         initialIndex: 0,
         count: 20,
-        passphrase: "",
+        passphrase: '',
       },
     },
   },
-  solidity: "0.8.4",
+  solidity: '0.8.4',
   paths: {
-    sources: "./contracts",
-    tests: "./test",
-    cache: "./cache",
-    artifacts: "./artifacts",
+    sources: './contracts',
+    tests: './test',
+    cache: './cache',
+    artifacts: './artifacts',
   },
   mocha: {
     timeout: 40000,
--- pwa/src/App.vue
+++ pwa/src/App.vue
@@ -1,125 +1,43 @@
 <script setup>
-import { RouterLink, RouterView } from "vue-router";
-import HelloWorld from "@/components/HelloWorld.vue";
+import { onMounted, ref } from 'vue';
+import Layout from './components/layout.vue';
+import NotConnectedForm from './components/not-connected-form.vue';
+import InputForm from './components/input-form.vue';
+
+const currentAccount = ref();
+
+async function checkIfWalletIsConnected() {
+  const { ethereum } = window;
+
+  if (!ethereum) {
+    console.log('Error: No ethereum window object');
+    return;
+  } else {
+    console.log('we have an ethereum object', ethereum);
+  }
+
+  const accounts = await ethereum.request({ method: 'eth_accounts' });
+
+  if (accounts.length !== 0) {
+    const account = accounts[0];
+    currentAccount.value = account;
+  } else {
+    console.log('No authorized accounts');
+  }
+}
+
+onMounted(() => {
+  checkIfWalletIsConnected();
+});
 </script>
 
 <template>
-  <header>
-    <img
-      alt="Vue logo"
-      class="logo"
-      src="@/assets/logo.svg"
-      width="125"
-      height="125"
-    />
-
-    <div class="wrapper">
-      <HelloWorld msg="You did it!" />
-
-      <nav>
-        <RouterLink to="/">Home</RouterLink>
-        <RouterLink to="/about">About</RouterLink>
-      </nav>
-    </div>
-  </header>
-
-  <RouterView />
+  <layout>
+    <not-connected-form v-model="currentAccount" v-if="!currentAccount" />
+    <input-form v-if="currentAccount" />
+  </layout>
 </template>
 
 <style>
-@import "@/assets/base.css";
-
-#app {
-  max-width: 1280px;
-  margin: 0 auto;
-  padding: 2rem;
-
-  font-weight: normal;
-}
-
-header {
-  line-height: 1.5;
-  max-height: 100vh;
-}
-
-.logo {
-  display: block;
-  margin: 0 auto 2rem;
-}
-
-a,
-.green {
-  text-decoration: none;
-  color: hsla(160, 100%, 37%, 1);
-  transition: 0.4s;
-}
-
-@media (hover: hover) {
-  a:hover {
-    background-color: hsla(160, 100%, 37%, 0.2);
-  }
-}
-
-nav {
-  width: 100%;
-  font-size: 12px;
-  text-align: center;
-  margin-top: 2rem;
-}
-
-nav a.router-link-exact-active {
-  color: var(--color-text);
-}
-
-nav a.router-link-exact-active:hover {
-  background-color: transparent;
-}
-
-nav a {
-  display: inline-block;
-  padding: 0 1rem;
-  border-left: 1px solid var(--color-border);
-}
-
-nav a:first-of-type {
-  border: 0;
-}
-
-@media (min-width: 1024px) {
-  body {
-    display: flex;
-    place-items: center;
-  }
-
-  #app {
-    display: grid;
-    grid-template-columns: 1fr 1fr;
-    padding: 0 2rem;
-  }
-
-  header {
-    display: flex;
-    place-items: center;
-    padding-right: calc(var(--section-gap) / 2);
-  }
-
-  header .wrapper {
-    display: flex;
-    place-items: flex-start;
-    flex-wrap: wrap;
-  }
-
-  .logo {
-    margin: 0 2rem 0 0;
-  }
-
-  nav {
-    text-align: left;
-    margin-left: -1rem;
-    font-size: 1rem;
-
-    padding: 1rem 0;
-    margin-top: 1rem;
-  }
-}
+@import './assets/base.css';
 </style>
