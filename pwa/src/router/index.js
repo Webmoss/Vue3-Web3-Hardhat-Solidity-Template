7dafce98b08747484ae02493f7b1ed002cd1875c
Craig Moss <craig@io.co.za>
2022-06-14 14:15:49 +0200

    Add pinia example

--- pwa/src/router/index.js
+++ pwa/src/router/index.js
@@ -1,12 +1,12 @@
-import { createRouter, createWebHistory } from "vue-router";
-import HomeView from "../views/HomeView.vue";
+import { createRouter, createWebHistory } from 'vue-router';
+import HomeView from '../views/HomeView.vue';
 
 const router = createRouter({
   scrollBehavior(to, from, savedPosition) {
     if (to.hash) {
       return window.scrollTo({
         top: document.querySelector(to.hash).offsetTop - 135,
-        behavior: "smooth",
+        behavior: 'smooth',
       });
     } else {
       return savedPosition || { x: 0, y: 0 };
@@ -15,17 +15,17 @@ const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
   routes: [
     {
-      path: "/",
-      name: "home",
+      path: '/',
+      name: 'home',
       component: HomeView,
     },
     {
-      path: "/about",
-      name: "about",
+      path: '/about',
+      name: 'about',
       // route level code-splitting
       // this generates a separate chunk (About.[hash].js) for this route
       // which is lazy-loaded when the route is visited.
-      component: () => import("../views/AboutView.vue"),
+      component: () => import('../views/AboutView.vue'),
     },
   ],
 });
